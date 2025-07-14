8e7d5952ab8d695910809c94e4112e91778933e7
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-07-14 20:45:34 +0900

    fear: init React TS + Vite project
    Set up a new project using React, TypeScript, and Vite. Added initial configuration files including ESLint, TypeScript configs, Vite config, and Tailwind CSS integration. Included basic source files, assets, and documentation.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Intern In Meta</h1>
+      <div>Project init</div>
+    </div>
+  )
+}
+
+export default App
